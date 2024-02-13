fbce216e4793891f3998fc703765a9f8353588be
borriellogiuann <136123294+borriellogiuann@users.noreply.github.com>
2024-02-13 10:24:11 +0100

    Initial commit

---
+++ SneakersInAir/SneakersInAirApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakersInAirApp.swift
+//  SneakersInAir
+//
+//  Created by Giovanni Borriello on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SneakersInAirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
