be88a35d24abee9badbe573f83bc68aa0e266ecc
D4fra <148436310+D4fra@users.noreply.github.com>
2024-02-20 10:18:08 +0100

    added searchBar

--- SneakersInAir/Views/ExploreView.swift
+++ SneakersInAir/Views/ExploreView.swift
@@ -7,12 +7,57 @@
 
 import SwiftUI
 
+
+
+
 struct ExploreView: View {
+    @State private var searchText: String = ""
     var body: some View {
-        Text("Explore")
+        SearchBar(text: $searchText)
     }
 }
-
 #Preview {
     ExploreView()
+}
+
+
+// Design della barra di ricerca
+struct SearchBar: View {
+    @Binding var text: String
+    var body: some View {
+        HStack{
+            TextField("Search", text: $text)
+                .padding(.leading, 34)
+                .padding(.trailing, 8)
+                .padding(.vertical, 8)
+                .overlay(
+                    HStack{
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.orange)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                            .padding(.trailing, 4)
+                        
+                    }
+                )
+                .overlay(
+                    HStack{
+                        Image(systemName: "mic.fill")
+                            .foregroundColor(.orange)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                            .padding(.trailing, 15)
+                            .padding(.leading, 4)
+                        
+                    }
+                )
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke()
+        
+        )
+        .padding()
+        
+        
+    }
 }
