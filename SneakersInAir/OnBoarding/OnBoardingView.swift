09f3fe4d157707d88acd2059c627fc3b4c125b8f
dodixwannacry <132576872+dodixwannacry@users.noreply.github.com>
2024-02-20 13:30:33 +0100

    On boarding
    v1

---
+++ SneakersInAir/OnBoarding/OnBoardingView.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+
+struct OnBoardingView: View {
+    @State private var pageIndex = 0
+    private let pages: [Page] = Page.samplePages
+    private let dotAppearance = UIPageControl.appearance()
+    
+    var body: some View {
+        TabView(selection: $pageIndex) {
+            ForEach(pages) { page in
+                VStack {
+                    Spacer()
+                    PageView(page: page)
+                    Spacer()
+                    if page == pages.last {
+                        Button("Sign up!", action: goToZero)
+                            .buttonStyle(.bordered)
+                            .tint(.customorange)
+                    } else {
+                        Button("next", action: incrementPage)
+                            .buttonStyle(.borderedProminent)
+                            .tint(.customorange)
+                    }
+                    Spacer()
+                }
+                .tag(page.tag)
+            }
+        }
+        .animation(.easeInOut, value: pageIndex)// 2
+        .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+        .tabViewStyle(PageTabViewStyle())
+        .onAppear {
+            dotAppearance.currentPageIndicatorTintColor = .black
+            dotAppearance.pageIndicatorTintColor = .gray
+        }
+    }
+    
+    func incrementPage() {
+        pageIndex += 1
+    }
+    
+    func goToZero() {
+        pageIndex = 0
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingView()
+    }
+}
